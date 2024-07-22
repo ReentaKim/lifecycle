61ad93740536308520000cc28fe1ce0759268003
Reenta <reenta.kim@gmail.com>
2024-07-22 17:07:19 +0900

    done!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let counter = 0;
+    const [counter2, setCounter2] = useState(0);
+    const [value, setValue] = useState(0);
+    const increase = () => {
+        counter = counter + 1;
+        setCounter2(counter2 + 1);
+        setValue(value + 2);
+        console.log('counter', counter, 'counter2 state는', counter2);
+    }
+
+    useEffect(() => {
+        console.log('useEffect 1 Fire!! : componentDidMount()')
+    }, []);
+
+    useEffect(() => {
+        console.log('useEffect 2 Fire!! : componentDidUpdate()', counter2, value)
+    }, [counter2, value]);
+
+    return (
+        <div className="App">
+            {console.log('render')}
+            <button onClick={increase}>버튼</button>
+        </div>
+    );
 }
 
 export default App;
